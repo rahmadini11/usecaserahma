cb81c3c2c702ca06a1ca559c503b50c6860759e4
rahmadini11 <rrhmaandini26@gmail.com>
2024-08-07 14:51:13 +0700

    pert 1-3

---
+++ javaapplication/ptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+class ptionPane {
+
+    static void showMessageDialog(Object object, String belajar_Java, String pesan, int INFORMATION_MESSAGE) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
